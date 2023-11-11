c1c676dd861073ce70c8efc82dd17e15877c2452
emerson marinho <emerson_marinhoaz@hotmail.com>
2023-11-11 18:36:12 -0300

    feat: added responsiveness

--- meupositivoHackaton/src/components/NavBar.tsx
+++ meupositivoHackaton/src/components/NavBar.tsx
@@ -2,16 +2,16 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav className="bg-[#00B5E2] p-4 flex justify-center items-center w-full h-12">
-      <div className="flex space-x-4 items-center justify-center">
-        <NavLink to="/notebooks" className="text-white hover:bg-[#2F393B] px-3 py-2">Notebooks</NavLink>
-        <NavLink to="/all-in-one" className="text-white hover:bg-[#2F393B] px-3 py-2">All in one</NavLink>
-        <NavLink to="/cellular" className="text-white hover:bg-[#2F393B] px-3 py-2">Celulares</NavLink>
-        <NavLink to="/tablets" className="text-white hover:bg-[#2F393B] px-3 py-2">Tablets</NavLink>
-        <NavLink to="/smart-home" className="text-white hover:bg-[#2F393B] px-3 py-2">Casa Inteligente</NavLink>
-        <NavLink to="/accessories" className="text-white hover:bg-[#2F393B] px-3 py-2">Acessórios</NavLink>
-        <NavLink to="/services" className="text-white hover:bg-[#2F393B] px-3 py-2">Serviços</NavLink>
-        <NavLink to="/soupositivo" className="text-white hover:bg-[#2F393B] px-3 py-2">#Sou Positivo</NavLink>
+    <nav className="bg-[#00B5E2] px-4 py-1 sm:py-1 flex justify-center items-center w-full ">
+      <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 items-center justify-center">
+        <NavLink to="/notebooks" className="text-white hover:bg-[#2F393B] px-3 py-2 text-xs sm:text-base">Notebooks</NavLink>
+        <NavLink to="/all-in-one" className="text-white hover:bg-[#2F393B] px-3 py-2 text-xs sm:text-base">All in one</NavLink>
+        <NavLink to="/celular" className="text-white hover:bg-[#2F393B] px-3 py-2 text-xs sm:text-base">Celulares</NavLink>
+        <NavLink to="/tablets" className="text-white hover:bg-[#2F393B] px-3 py-2 text-xs sm:text-base">Tablets</NavLink>
+        <NavLink to="/smart-home" className="text-white hover:bg-[#2F393B] px-3 py-2 text-xs sm:text-base">Casa Inteligente</NavLink>
+        <NavLink to="/accessories" className="text-white hover:bg-[#2F393B] px-3 py-2 text-xs sm:text-base">Acessórios</NavLink>
+        <NavLink to="/services" className="text-white hover:bg-[#2F393B] px-3 py-2 text-xs sm:text-base">Serviços</NavLink>
+        <NavLink to="/soupositivo" className="text-white hover:bg-[#2F393B] px-3 py-2 text-xs sm:text-base">#Sou Positivo</NavLink>
       </div>
     </nav>
   );
